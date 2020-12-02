f6d244333cd2ee78ae1091c57c10e882be2220e9
Avik Mukherjee <mukherjee.avik852@gmail.com>
2020-12-02 18:45:56 +0530

    Setter Injection Through Autowiring

---
+++ Setter Injection Through Autowiring/annotationdemo/src/main/java/com/spring/poc/annotationdemo/TennisCoach.java
@@ -0,0 +1,37 @@
+package com.spring.poc.annotationdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("thatSillyCoach")
+public class TennisCoach implements Coach {
+
+	private FortuneService fortuneService;
+	
+	public TennisCoach() {
+		
+	}
+	
+	/*
+	@Autowired
+	public TennisCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+	*/
+	
+	@Autowired
+	private void setFortuneService(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+	
+	@Override
+	public String getDailtyWorkout() {
+		return "Practice backhand volley";
+	}
+
+	@Override
+	public String getDailyFortume() {
+		return fortuneService.getFortune();
+	}
+
+}
